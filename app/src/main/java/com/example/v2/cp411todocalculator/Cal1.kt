78eec28cff2e24593748a90d5a67aa33c9562c1f
1337menohax <vivu493@gmail.com>
2018-11-05 19:59:24 -0800

    1.0

---
+++ app/src/main/java/com/example/v2/cp411todocalculator/Cal1.kt
@@ -0,0 +1,40 @@
+package com.example.v2.cp411todocalculator
+
+import android.support.v4.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.cal1.*
+import kotlinx.android.synthetic.main.cal1.view.*
+
+class Cal1 : Fragment() {
+    internal var userBill = 0.0
+    internal var userTip = 0.0
+    internal var userTax = 0.0
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val rootView = inflater.inflate(R.layout.cal1, container, false)
+
+
+        rootView.btCalculate?.setOnClickListener {
+            if (!etBillAmount.text.toString().isEmpty()) {
+                userBill = java.lang.Double.parseDouble(etBillAmount.text.toString())
+            }
+            if (!etTip.text.toString().isEmpty()) {
+                userTip = java.lang.Double.parseDouble(etTip.text.toString())
+            }
+            if (!etSaleTax.text.toString().isEmpty()) {
+                userTax = java.lang.Double.parseDouble(etSaleTax.text.toString())
+            }
+            tvTotal.text = "$" + String.format("%.2f", calculate(userBill, userTip, userTax))
+        }
+        return rootView
+
+    }
+
+
+    private fun calculate(cost: Double, tip: Double, tax: Double): Double {
+        return cost + cost * (tip / 100) + cost * (tax / 100)
+    }
+}
